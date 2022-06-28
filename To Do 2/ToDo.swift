e196070402e212b6bf9e12b5a61bd0a723c5a8ec
akjorns <akjorns@icloud.com>
2022-06-28 18:29:09 -0500

    Initial Commit

---
+++ To Do 2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  To Do 2
+//
+//  Created by Alex Jorns on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ To Do 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do 2
+//
+//  Created by Alex Jorns on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
